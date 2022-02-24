c7fb343a8652e48842f3d42606d1e8f91d3ac440
User <User@DESKTOP-QAT0BJ3>
2022-02-24 18:05:49 +0700

    Добавьте файлы проекта.

---
+++ RomanNumberException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Laba2
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException() : base() { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
